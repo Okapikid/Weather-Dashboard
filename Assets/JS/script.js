1ac7240ea45720290f73b2d2fb79a7fe927ca158
Auggie <miaugus@gmail.com>
2022-07-17 22:50:27 -0400

    updates to HTML and JS, getting errors in API call

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,9 @@
-const currentTime = moment().format('MMMM Do, YYYY');
+let searchedCity = '';
+let stuff
+let cityCall
+let storedArray = [];
 
+const currentTime = moment().format('MMMM Do, YYYY');
 const day1 = moment().add(1, "days").format("MMMM Do, YYYY");
 const day2 = moment().add(2, "days").format("MMMM Do, YYYY");
 const day3 = moment().add(3, "days").format("MMMM Do, YYYY");
@@ -11,3 +15,68 @@ $('#date2').text(day2);
 $('#date3').text(day3);
 $('#date4').text(day4);
 $('#date5').text(day5);
+
+let searchHistory = JSON.parse(localStorage.getItem(`previousSearches`))
+if (searchHistory === null) {
+  searchHistory = [`Cincinnati`]
+}
+
+storedArray = searchHistory;
+for (let i = 0; i < storedArray.length; i++) {
+  if (searchHistory[i] !== null) {
+    const city = `<div id='${searchHistory[i]}' class="city">${storedArray[i]}</div>`
+    $(`searchedCities`).append(city);
+  }
+}
+
+const searchedClick = function () {
+  $(".city").on("click", function () {
+    const cityCard = $(this).text()
+    searchedCity = cityCard
+    cityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`
+    $("#title").text(`${searchedCity}`)
+    getWeather(cityAPI, stuff);
+  })
+}
+
+const getWeather = function (cityInfo, stuff) {
+  fetch(cityInfo).then(function (res) {
+    return res.json()
+  }).then(function (data) {
+    stuff = data
+    const cityApiPull = `https://api.openweathermap.org/data/3.0/onecall?lat=${stuff.coord.lat}&lon=${stuff.coord.lon}&appid=da46faa4e2f031a23137502340090190`
+    fetch(cityApiPull).then(function (res) {
+      return res.json().then(function (data) {
+        $("#temp").text(`Temp: ${data.current.temp}`)
+        $("#wind").text(`Wind: ${data.wind_speed} MPH`)
+        $("#humidity").text(`Humidity ${data.current.humidity}%`)
+        $("#uv").text(`UV Index: ${data.current.uvi}`)
+      })
+    })
+  })
+}
+
+$('#submit-btn').on('click', function (e) {
+  searchedCity = $(`#search-bar`).val()
+
+  const city = `<div id="${searchedCity}" class="city">${searchHistory}</div>`
+  $("#searchedCities").append(city)
+  $("#title").text(`#{searchedCity}`)
+
+  storedArray.push(searchedCity);
+  localStorage.setItem("previousSearches", JSON.stringify(storedArray))
+
+  let cityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`
+  $(`.city`).on('click', function () {
+    const cityCard = $(this).text()
+    searchedCity = cityCardcityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`
+
+    $('#title').text(`${searchedCity}`)
+    getWeather(cityAPI, stuff)
+  })
+  getWeather(cityAPI, stuff)
+  e.preventDefault();
+})
+
+console.log(day1);
+console.log(searchedCity);
