251615e5c194d5436538d4e80b2e2583d9f5d0ce
Auggie <miaugus@gmail.com>
2022-08-27 00:18:12 -0400

    updated Readme

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,83 +1,87 @@
-let searchedCity = '';
-let stuff
-let cityCall
+let searchedCity = "";
+let submitButtonEl = document.getElementById("");
+let stuff;
+let cityCall;
 let storedArray = [];
 
-const currentTime = moment().format('MMMM Do, YYYY');
+const currentTime = moment().format("MMMM Do, YYYY");
 const day1 = moment().add(1, "days").format("MMMM Do, YYYY");
 const day2 = moment().add(2, "days").format("MMMM Do, YYYY");
 const day3 = moment().add(3, "days").format("MMMM Do, YYYY");
 const day4 = moment().add(4, "days").format("MMMM Do, YYYY");
 const day5 = moment().add(5, "days").format("MMMM Do, YYYY");
 
-$('#date1').text(day1);
-$('#date2').text(day2);
-$('#date3').text(day3);
-$('#date4').text(day4);
-$('#date5').text(day5);
+$("#date1").text(day1);
+$("#date2").text(day2);
+$("#date3").text(day3);
+$("#date4").text(day4);
+$("#date5").text(day5);
 
-let searchHistory = JSON.parse(localStorage.getItem(`previousSearches`))
+let searchHistory = JSON.parse(localStorage.getItem(`previousSearches`));
 if (searchHistory === null) {
-  searchHistory = [`Cincinnati`]
+  searchHistory = [`Cincinnati`];
 }
 
 storedArray = searchHistory;
 for (let i = 0; i < storedArray.length; i++) {
   if (searchHistory[i] !== null) {
-    const city = `<div id='${searchHistory[i]}' class="city">${storedArray[i]}</div>`
+    const city = `<div id='${searchHistory[i]}' class="city">${storedArray[i]}</div>`;
     $(`searchedCities`).append(city);
   }
 }
 
 const searchedClick = function () {
   $(".city").on("click", function () {
-    const cityCard = $(this).text()
-    searchedCity = cityCard
-    cityAPI = `https://api.openweathermap.org/data/2.5/onecall?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`
-    $("#title").text(`${searchedCity}`)
+    const cityCard = $(this).text();
+    searchedCity = cityCard;
+    cityAPI = `https://api.openweathermap.org/data/2.5/onecall?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`;
+    $("#title").text(`${searchedCity}`);
     getWeather(cityAPI, stuff);
-  })
-}
+  });
+};
 searchedClick();
 
 const getWeather = function (cityInfo, stuff) {
-  fetch(cityInfo).then(function (res) {
-    return res.json()
-  }).then(function (data) {
-    stuff = data
-    const cityApiPull = `https://api.openweathermap.org/data/2.5/onecall?lat=${stuff.coord.lat}&lon=${stuff.coord.lon}&appid=da46faa4e2f031a23137502340090190`
-    fetch(cityApiPull).then(function (res) {
-      return res.json().then(function (data) {
-        $("#temp").text(`Temp: ${data.temp}`)
-        $("#wind").text(`Wind: ${data.wind_speed} MPH`)
-        $("#humidity").text(`Humidity ${data.humidity}%`)
-        $("#uv").text(`UV Index: ${data.current.uvi}`)
-      })
+  fetch(cityInfo)
+    .then(function (res) {
+      return res.json();
     })
-  })
-}
+    .then(function (data) {
+      stuff = data;
+      const cityApiPull = `https://api.openweathermap.org/data/2.5/onecall?lat=${stuff.coord.lat}&lon=${stuff.coord.lon}&appid=da46faa4e2f031a23137502340090190`;
+      fetch(cityApiPull).then(function (res) {
+        return res.json().then(function (data) {
+          $("#temp").text(`Temp: ${data.temp}`);
+          $("#wind").text(`Wind: ${data.wind_speed} MPH`);
+          $("#humidity").text(`Humidity ${data.humidity}%`);
+          $("#uv").text(`UV Index: ${data.current.uvi}`);
+        });
+      });
+    });
+};
 
-$('#submit-btn').on('click', function (e) {
-  searchedCity = $(`#search-bar`).val()
+$("#submit-btn").on("click", function (e) {
+  searchedCity = $(`#search-bar`).val();
 
-  const city = `<div id="${searchedCity}" class="city">${searchHistory}</div>`
-  $("#searchedCities").append(city)
-  $("#title").text(`#{searchedCity}`)
+  const city = `<div id="${searchedCity}" class="city">${searchHistory}</div>`;
+  $("#searchedCities").append(city);
+  $("#title").text(`#{searchedCity}`);
 
   storedArray.push(searchedCity);
-  localStorage.setItem("previousSearches", JSON.stringify(storedArray))
+  localStorage.setItem("previousSearches", JSON.stringify(storedArray));
 
-  let cityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`
-  $(`.city`).on('click', function () {
-    const cityCard = $(this).text()
-    searchedCity = cityCardcityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`
+  let cityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`;
+  $(`.city`).on("click", function () {
+    const cityCard = $(this).text();
+    searchedCity =
+      cityCardcityAPI = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=da46faa4e2f031a23137502340090190`;
 
-    $('#title').text(`${searchedCity}`)
-    getWeather(cityAPI, stuff)
-  })
-  getWeather(cityAPI, stuff)
+    $("#title").text(`${searchedCity}`);
+    getWeather(cityAPI, stuff);
+  });
+  getWeather(cityAPI, stuff);
   e.preventDefault();
-})
+});
 
 console.log(day1);
 console.log(searchedCity);
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Weather-Dashboard
+
+## Description
+
+This Weather Dashboard was intended to retrieve weather data for cities through the third-party OpenWeather One Call API. <br>
+<br>
+
+## Table of Contents
+
+- [GitHub](#github)
+
+## Github
+
+The GitHub repository for this project can be found here: [link to GitHub repository](https://github.com/Okapikid/Weather-Dashboard)
